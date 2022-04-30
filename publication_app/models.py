abff389dfe703221d9717db74b461174aa019ad2
Artem Shcherban <yammyk1992@gmail.com>
2022-05-01 00:23:36 +0300

    update .gitignore

--- publication_app/models.py
+++ publication_app/models.py
@@ -8,5 +8,5 @@ class Post(models.Model):
     title = models.CharField(max_length=256, unique=False, blank=False, null=False)
     text = models.TextField(blank=False, null=False)
     is_public = models.BooleanField(default=True)
-    image = models.ImageField(null=True)
+    image = models.ImageField(null=True, blank=True)
 
--- publication_app/views.py
+++ publication_app/views.py
@@ -6,7 +6,7 @@ from .models import Post
 
 
 def main_page(request):
-    posts = Post.objects.filter(is_public=True).order_by('-created_at', '-id',).all()
+    posts = Post.objects.filter(is_public=True).order_by('created_at', 'id',).all()
 
     contex = {'title': 'HI ALL', 'posts': posts}
     return render(request, 'main_page.html', contex)
