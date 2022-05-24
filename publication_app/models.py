70b24fc4521cfbdfc2bc0e73c119752ded2089a4
Artem Shcherban <100095205+yammyk1992@users.noreply.github.com>
2022-05-25 01:30:43 +0300

    Add files via upload

--- publication_app/models.py
+++ publication_app/models.py
@@ -6,13 +6,14 @@ from django.urls import reverse
 
 class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
-    title = models.CharField(max_length=256, unique=False, blank=False, null=False, verbose_name='Заголовок')
+    title = models.CharField(max_length=256, verbose_name='Заголовок')
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
     nationality = models.TextField(max_length=256, unique=False, blank=False, null=False, verbose_name='Национальность')
     content = models.TextField(blank=True, null=False, verbose_name='Пост')
     is_public = models.BooleanField(default=True, verbose_name='Публикация')
     image = models.ImageField(null=True, blank=True, verbose_name='Фото')
     category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категории')
+
     # для вывода в режиме shell терминал заголовков объектов из базы данных
 
     def __str__(self):
@@ -27,6 +28,16 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
+
+#
+# class PostImage(models.Model):
+#     # id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+#     post = models.ForeignKey(Post, default=None, on_delete=models.CASCADE)
+#     image = models.ImageField(null=True, blank=True, verbose_name='Фото')
+#     title = models.CharField(max_length=256)
+#
+#     def __str__(self):
+#         return self.post
 
 
 class Category(models.Model):
