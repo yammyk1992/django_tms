82ea4816c6f28b15ca79f0eab6f00f489c655fa6
yammyk <yammyk1992@gmail.com>
2022-07-05 01:09:36 +0300

    update my_django/settings.py/2

--- my_django/settings.py
+++ my_django/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
+    # 'debug_toolbar',
     'publication_app.apps.PublicationAppConfig',
     'drf_spectacular',
     'rest_framework',
